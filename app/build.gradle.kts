d7a84cd15cc26bdba83ce556d1b82cc1fc3111e6
JamesHimselff <pv.borras.jaume@gmail.com>
2024-05-07 18:31:51 +0200

    Volley Fitted

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,7 @@ dependencies {
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-database")
     implementation("com.google.firebase:firebase-storage")
-    implementation ("com.squareup.retrofit2:retrofit:(2.9.0)")
-    implementation ("com.squareup.retrofit2:converter-gson:(2.9.0)")
+    implementation("com.android.volley:volley:1.2.1")
 
 
 }
